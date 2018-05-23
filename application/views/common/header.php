582eccd3dc16f325a641f0678590721ed2608dec
webworks <aatish.gore@wwindia.com>
2018-05-23 17:31:52 +0530

    CRM Header chanegs

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -329,6 +329,13 @@
                     </a>
                 </li>
                 <?php }?>
+                <?php if(in_array($this->session->userdata('admin_type'),array('Super admin'))) {?>
+                    <li>
+                        <a href="<?php echo site_url('crm')?>">
+                            CRM
+                        </a>
+                    </li>
+                <?php }?>
 
 			</ul>
 		</div>
