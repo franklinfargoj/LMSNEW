1a75a42536f1d56581781d5b6641caee9bccb4b8
webwerks <madhura.bhagwat@neosofttech.com>
2018-05-25 15:25:07 +0530

    download sample files in customer retention and user upload

--- application/views/middle/customer_retention_import.php
+++ application/views/middle/customer_retention_import.php
@@ -4,7 +4,12 @@
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    
+<style>
+.vl {
+    border-left: 1px solid #d8d8d8;
+    height: 200px;
+}  
+</style>
 </head>
 <body>
 <div class="page-title">
@@ -17,13 +22,19 @@
 	<input type="hidden" name="<?php echo $this->security->get_csrf_token_name();?>" value="<?php echo $this->security->get_csrf_hash();?>">
 <table>
 <tr>
-<td> Choose your file: </td>
-<td><input type="file" class="form-control" name="file" id="file"  align="center"/>
-	<button type="submit" name="Import" class="btn btn-info"  >   Save  </button>
+<td>Choose your file:<hr><input type="file" class="form-control" name="file" id="file"  align="center"/>
+
 	<div class="valid-msg"><span>*</span>Only csv</div>
+	<button type="submit" name="Import" class="btn btn-info"  >   Save  </button>
 </td>
+<td><div class="vl"></div></td>
 
-
+<td><div class="upload-xl">
+                <a href="<?php echo base_url('uploads/sample/Customer-Retention_v1.4.xlsx')?>">
+                    <img src="<?php echo base_url().ASSETS;?>images/excel-img.png" alt="excel">
+                    <span>Download Sample File</span>
+                </a>
+            </div></td>
 </tr>
 
 </table> 
