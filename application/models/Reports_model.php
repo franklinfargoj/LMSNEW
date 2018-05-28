bad74e844f175a497b61c393528755bb8abd5193
webworks <aatish.gore@wwindia.com>
2018-05-28 19:44:31 +0530

    Import CSV new Tab

--- application/models/Reports_model.php
+++ application/models/Reports_model.php
@@ -53,5 +53,40 @@ class Reports_model extends CI_Model{
             return true;
         }
     }
+    public function insert_uploaded_data_all($data,$escape = NULL)
+    {
+        foreach ($data as $i => $data1) {
+            $data_zone[] = array(
+                "name" => $data1['zone_name'],
+                "code" => $data1['zone_code']
+            );
+        }
+        foreach ($data as $i => $data2) {
+            $data_state[] = array(
+                "name" => $data2['state_name'],
+                "code" => $data2['state_code'],
+                "zone_code" => $data2['zone_code']
+            );
+        }
+        foreach ($data as $i => $data3) {
+            $data_district[] = array(
+                "name" => $data3['district_name'],
+                "code" => $data3['district_code'],
+                "state_code" => $data3['state_code']
+            );
+        }
+        foreach ($data as $i => $data4) {
+            $data_branch[] = array(
+                "name" => $data4['branch_name'],
+                "code" => $data4['branch_code'],
+                "district_code" => $data4['district_code']
+            );
+        }
+        $this->db->insert_batch("db_zone",$data_zone);
+        $this->db->insert_batch("db_state",$data_state);
+        $this->db->insert_batch("db_district",$data_district);
+        $this->db->insert_batch("db_branch",$data_branch);
+        return true;
+    }
 
 }
