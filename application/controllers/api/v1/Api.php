d5a54b888f253478a4e194cabade82c1014bd901
Aatish Gore <aatish.gore@wwindia.com>
2018-06-15 15:11:42 +0530

    get designation issue fixation

--- application/controllers/api/v1/Api.php
+++ application/controllers/api/v1/Api.php
@@ -1765,7 +1765,7 @@ $arrData['unassigned_leads_count'] = $this->Lead->unassigned_status_count($selec
                 'designation_name' => $records['designation'],
                 'mobile' => $records['contact_no'],
                 'email_id' => $records['email_id'],
-                'designation' => get_designation($records['designation_id']),
+                'designation' => $records['designation'],
                 //'dept_id' => $records['dept_id']
             );
 
@@ -2011,7 +2011,7 @@ $arrData['unassigned_leads_count'] = $this->Lead->unassigned_status_count($selec
                 'designation_name' => $records['designation'],
                 'mobile' => $records['contact_no'],
                 'email_id' => $records['email_id'],
-                'designation' => get_designation($records['designation_id']),
+                'designation' => $records['designation'],
                 //'dept_id' => $records['dept_id']
             );
 
